b78dbb71aabd258a4c4699fac3a8e7c792e0e53a
Linus Yang <laokongzi@gmail.com>
2018-04-04 05:01:37 +0800

    Fix macOS build

--- library/sha256.c
+++ library/sha256.c
@@ -144,7 +144,7 @@ void mbedtls_sha256_starts( mbedtls_sha256_context *ctx,
 #endif
 
 #if !defined(MBEDTLS_SHA256_PROCESS_ALT)
-static const uint32_t K[] =
+static const uint32_t K[] __attribute__((unused)) =
 {
     0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
     0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
